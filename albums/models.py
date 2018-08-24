d60cf1efa6f76939780708e25347fa5677db6c77
Samuelson <591168@bah.com>
2018-08-24 13:20:07 -0400

    Added models for albums

---
+++ albums/models.py
@@ -0,0 +1,128 @@
+from django.db import models
+
+# Create your models here.
+class Artist(models.Model):
+    name = models.CharField()
+
+class PrimaryGenre(models.Model):
+    name = models.CharField(
+        unique=True
+    )
+
+class SubGenre(models.Model):
+    name = models.CharField(
+        unique=True
+    )
+    
+class Rating(models.Model):
+    score = models.IntegerField(
+        null=True,
+        blank=True
+    )
+
+    listen = models.IntegerField(
+        null=True,
+        blank=True
+    )
+
+    album = models.ForeignKey(
+        to='Album',
+        null=False,
+        blank=False,
+        on_delete=models.PROTECT
+    )
+
+class Album(models.Model):
+    name = models.CharField(
+        blank=False,
+        null=False
+    )
+
+    artist = models.ForeignKey(
+        to='Artist',
+        related_name="albums",
+        blank=False,
+        null=False,
+        on_delete=models.PROTECT
+    )
+
+    order = models.IntegerField(
+        null=True,
+        blank=True
+    )
+
+    chart = models.IntegerField(
+        null=True,
+        blank=True
+    )
+
+    row = models.IntegerField(
+        null=True,
+        blank=True
+    )
+
+    date_finished = models.DateField(
+        blank=True,
+        null=True
+    )
+
+    primary_genre = models.ForeignKey(
+        to='PrimaryGenre',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL
+    )
+
+    sub_genre = models.ManyToManyField(
+        'SubGenre',
+        blank=True,
+        null=True
+    )
+
+    time_length = models.DurationField(
+        null=True,
+        blank=True
+    )
+
+    release_date = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    album_art = models.URLField(
+        null=True,
+        blank=True
+    )
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def time_check(self):
+        if self.time_length != "":
+            return False
+        else:
+            return True
+
+    def release_date_check(self):
+        if self.release_date != "":
+            return False
+        else:
+            return True
+
+    def album_art_check(self):
+        if self.album_art != "":
+            return False
+        else:
+            return True
+
+    def all_info_found(self):
+        if not self.album_art_check() and not self.release_date_check() and not self.time_length_check():
+            return True
+        else:
+            return False
+    
+    def in_queue(self):
+        if self.date_finished:
+            return False
+        else:
+            return True
--- music/settings.py
+++ music/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h%62ka(@g-2k%ch^1ue28^9$s8^e90s&i^jkg0(1f)2e8o4z(('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'music.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
