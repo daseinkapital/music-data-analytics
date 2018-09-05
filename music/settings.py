95e666d9b9c71c0881e0b11b465785ee216ad7a8
Samuelson <591168@bah.com>
2018-09-05 11:23:53 -0400

    Switching basedir

--- music/settings.py
+++ music/settings.py
@@ -17,7 +17,7 @@ import django_heroku
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
